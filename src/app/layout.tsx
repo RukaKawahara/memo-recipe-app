57ed7236ef80d95676a7cf4b7ad332d627657e50
河原　瑠花 <rukacode22@gmail.com>
2025-09-28 10:34:44 +0900

    - add: アイコンデザインを設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,10 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?display=swap&family=Epilogue:wght@400;500;700;900&family=Noto+Sans:wght@400;500;700;900"
         />
+        <link rel="icon" href="/icons/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icons/icon-192x192.png" type="image/png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-180x180.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         <div className="app-container">
